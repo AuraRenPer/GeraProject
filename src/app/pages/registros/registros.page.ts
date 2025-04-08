635e875951c3b5a910e9b757d03ec8bf6f70630c
AuraRenata812 <becaria.aura@mideservices.com>
2025-04-07 20:35:43 -0600

    eliminar

--- src/app/pages/registros/registros.page.ts
+++ src/app/pages/registros/registros.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RegistroService } from 'src/app/core/registro.service';
 import { Registro } from 'src/app/models/registro.model';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registros',
@@ -13,7 +14,8 @@ export class RegistrosPage {
 
   constructor(
     private registroService: RegistroService,
-    private router: Router
+    private router: Router,
+    private alertCtrl: AlertController
   ) {}
 
   ionViewWillEnter() {
@@ -26,14 +28,31 @@ export class RegistrosPage {
 
   agregar() {
     this.router.navigate(['/registros/registro-form']);
-}
+  }
 
   editar(registro: Registro) {
     this.router.navigate(['/registros/registro-form', registro.id]);
   }
 
   async eliminar(id: number) {
-    await this.registroService.delete(id);
-    this.cargarRegistros();
+    const alerta = await this.alertCtrl.create({
+      header: '¿Eliminar?',
+      message: '¿Estás seguro de que quieres eliminar este registro?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Eliminar',
+          handler: async () => {
+            await this.registroService.delete(id);
+            this.cargarRegistros(); // Actualiza la lista
+          }
+        }
+      ]
+    });
+
+    await alerta.present();
   }
 }
